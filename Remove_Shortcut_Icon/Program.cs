2de13897dacd44b2bc05bc304a8fe536c084f98b
YimuQr <161135845+YimuQrrr@users.noreply.github.com>
2024-08-04 17:46:30 +0800

    Add files via upload

---
+++ Remove_Shortcut_Icon/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.Security.Principal;
+
+namespace Remove_Shortcut_Icon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Path = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Icons";
+            string Name = "29";
+            string Data = @"%windir%\System32\shell32.dll,51";
+
+            using (RegistryKey Key = Registry.LocalMachine.CreateSubKey(Path))
+            Key.SetValue(Name, Data, RegistryValueKind.String);
+
+            foreach (var process in Process.GetProcessesByName("explorer")) { process.Kill(); }
+            Process.Start("explorer.exe");
+        }
+    }
+}
